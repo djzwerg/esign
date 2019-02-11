b9337c4e7cc6a4b6eeca5127e83dbc85cc402626
Adam Weiss <adam@greatmatter.com>
2019-02-11 09:30:30 -0800

    Add Signature Pad as CDN. Remove dependency on local Signature Pad library. Add jquery_update module as dependency. Add file formats as an option.

--- js/esign.js
+++ js/esign.js
@@ -34,10 +34,10 @@
       // Instantiate the signaturepad itself.
       settings.onEnd = function () {
         // When a signature is done being signed, set the hidden field to contain the data.
-        signatureCapture.val(signaturePad.toDataURL());
+        signatureCapture.val(signaturePad.toDataURL(settings.toDataURL));
 
         // Also set the data-signature value.
-        thisContainer.find('.esign_panel').attr("data-signature", signaturePad.toDataURL());
+        thisContainer.find('.esign_panel').attr("data-signature", signaturePad.toDataURL(settings.toDataURL));
         thisContainer.find('.signature-created').val(Math.ceil(Date.now() / 1000));
       };
       signaturePad = new SignaturePad(canvas, settings);
@@ -52,24 +52,24 @@
         javascript:signaturePad.clear();
         signatureCapture.val("");
         thisContainer.find('.esign_panel').attr('data-signature', '');
-        resizeCanvas(canvas, signaturePad, signatureCapture)
+        resizeCanvas(canvas, signaturePad, signatureCapture, settings);
       });
 
       // Call the "resize" function for high-DPI screens.
-      resizeCanvas(canvas, signaturePad, signatureCapture)
+      resizeCanvas(canvas, signaturePad, signatureCapture, settings);
 
-      $(window).on("orientationchange",function () {
-        resizeCanvas(canvas, signaturePad, signatureCapture);
+      $(window).on('orientationchange', function () {
+        resizeCanvas(canvas, signaturePad, signatureCapture, settings);
       });
     });
   }
 
-  function resizeCanvas(canvas, signaturePad, signatureCapture) {
+  function resizeCanvas(canvas, signaturePad, signatureCapture, settings) {
     // When zoomed out to less than 100%, for some very strange reason,
     // some browsers report devicePixelRatio as less than 1
     // and only part of the canvas is cleared then.
     if (!signaturePad.isEmpty()) {
-      signatureCapture.val(signaturePad.toDataURL());
+      signatureCapture.val(signaturePad.toDataURL(settings.toDataURL));
     }
     var ratio = Math.max(window.devicePixelRatio || 1, 1);
     canvas.width = canvas.offsetWidth * ratio;
